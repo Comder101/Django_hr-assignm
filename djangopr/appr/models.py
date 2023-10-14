c2da1c3b74edbc0147ec3af649cdf4874dae57a4
Pratik Vinayak More <C2K20106742@pictsctr.onmicrosoft.com>
2023-10-14 11:15:12 +0530

    db migrations and serializers

--- djangopr/appr/models.py
+++ djangopr/appr/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Transaction(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    transaction_id = models.CharField(max_length=255)
+
 # Create your models here.
